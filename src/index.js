8cead4365bb0ca0000a9d01807fd9669c4b938b6
dev-chloe <chloekim.4969@gmail.com>
2022-07-26 19:22:25 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ const plus = document.getElementById("plus_btn");
 const minus = document.getElementById("minus_btn");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const counterReducer = (count = 0, action) => {
-  console.log(count, action)
   if (action.type === "PLUS") {
     return count + 1;
   } else if (action.type === "MINUS") {
@@ -28,11 +29,18 @@ const counterReducer = (count = 0, action) => {
 
 const countStore = legacy_createStore(counterReducer);
 
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handlePlus = () => {
+  countStore.dispatch({ type: "PLUS" })
+}
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" })
+}
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);

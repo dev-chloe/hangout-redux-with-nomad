5eaf570aa3273eaeb07440bbcfec7d8deabceab4
dev-chloe <chloekim.4969@gmail.com>
2022-07-26 17:52:17 +0900

    Vanilla Counter

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Vanilla-Redux and React-Redux
 
+Learning Vanilla-Redux and React-Redux
+
 ## Project
 
 ### How to run?
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const plus = document.getElementById("plus_btn");
+const minus = document.getElementById("minus_btn");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+
+const undateText = () => {
+  number.innerText = count;
+}
+
+const handlePlus = () => {
+  count = count + 1;
+  undateText();
+}
+const handleMinus = () => {
+  count = count-1;
+  undateText();
+}
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
